2a84f9ef068e97c6a70f59d941049cba642820f8
Joe Wemyss <joewemyss3@gmail.com>
2015-09-14 18:52:18 +0100

    Scaffolded site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,36 @@
 module.exports = function(grunt){
     grunt.initConfig({
-        pkg:{grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
         concat:{
-            options:
+            options:{
                 separator: '\n\n'
             },
             dist:{
-                src: ['src/js/**/*.js'],
-                dest: 'build/js/application.js'
+                src: ['src/app.js','src/js/*.js','src/js/**/*.js'],
+                dest: 'src/build/js/application.js'
             },
             deps:{
                 src: [
                     'bower_components/jquery/dist/jquery.js',
-                    'bower_components/bootstrap/dist/js/bootstrap.js'
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
                 ],
-                dest: 'build/js/application_deps.js'
+                dest: 'src/build/js/application-deps.js'
+            },
+            cssDeps:{
+                src: [
+                    'bower_components/bootstrap/dist/css/bootstrap.css',
+                    'bower_components/bootstrap/dist/css/bootstrap-theme.css'
+                ],
+                dest: 'src/build/styles/application-style.css'
             }
         },
         watch:{
-            files:['src/app.js', 'src/js/**/*.js'],
-            tasks:[concat]
+            files:['src/js/**/*.js', 'src/app.js', 'src/js/*.js'],
+            tasks:['concat:dist']
         }
     });
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
+    grunt.registerTask('build', ['concat:deps', 'concat:cssDeps']);
 };
