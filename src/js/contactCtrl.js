b6aa8bdea8da5e89db385a04a55cf0311727a5e1
Joe Wemyss <joewemyss3@gmail.com>
2015-10-28 23:44:16 +0000

    form validation

--- src/js/contactCtrl.js
+++ src/js/contactCtrl.js
@@ -1,7 +1,22 @@
 app.controller('ContactCtrl', ['$scope', function ($scope) {
-    $scope.messageSent = false;
-    $scope.sendMessage = function(){
-        $scope.messageSent = true;
-    }
-
+	$scope.checkEmail = function () {
+		if($scope.contactForm.emailInput.$valid){
+			$('#submit').removeClass('disabled');
+			$('#emailInput').removeClass('contactFormInvalid');
+		} else {
+			$('#submit').addClass('disabled');
+			$('#emailInput').addClass('contactFormInvalid');
+		}
+	};
+	$scope.sendMessage = function () {
+		if($('#submit').hasClass('disabled')){
+			return;
+		}
+		$('p.confirmMessage').show();
+	};
+	$scope.validateEmail = function(){
+		if($scope.contactForm.emailInput.$invalid){
+			$('#emailInput').addClass('contactFormInvalid');
+		}
+	}
 }]);
